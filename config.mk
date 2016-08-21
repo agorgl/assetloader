f4e021e896b404105acc13d78dabd288a5ae2f8f
TheArtist <agorglouk@gmail.com>
2016-08-21 17:20:19 +0300

    Added demo make target

--- config.mk
+++ config.mk
@@ -1,4 +1,9 @@
 PRJTYPE = StaticLib
 ifeq ($(OS), Windows_NT)
-DEFINES += _CRT_SECURE_NO_WARNINGS _CRT_SECURE_NO_DEPRECATE
+	DEFINES += _CRT_SECURE_NO_WARNINGS _CRT_SECURE_NO_DEPRECATE
 endif
+
+DEMODIR = demo
+demo: build
+	@$(MAKE) -C $(strip $(DEMODIR)) -f $(MKLOC) clean
+	@$(MAKE) -C $(strip $(DEMODIR)) -f $(MKLOC) run
