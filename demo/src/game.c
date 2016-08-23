7b465a334175ae2aa1d0b829b79c4a737b240b02
TheArtist <agorglouk@gmail.com>
2016-08-23 17:17:13 +0300

    Added OpenGL debug callback

--- demo/src/game.c
+++ demo/src/game.c
@@ -9,6 +9,20 @@
 #include <stdio.h>
 #include <assets/assetload.h>
 
+static void gl_debug_proc(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* user_param)
+{
+    (void) source;
+    (void) id;
+    (void) severity;
+    (void) length;
+    (void) user_param;
+
+    if (type == GL_DEBUG_TYPE_ERROR) {
+        fprintf(stderr, "%s", message);
+        exit(1);
+    }
+}
+
 static void on_key(struct window* wnd, int key, int scancode, int action, int mods)
 {
     (void)scancode; (void)mods;
@@ -146,6 +160,9 @@ void game_init(struct game_context* ctx)
     memset(&wnd_callbacks, 0, sizeof(struct window_callbacks));
     wnd_callbacks.key_cb = on_key;
     window_set_callbacks(ctx->wnd, &wnd_callbacks);
+
+    /* Setup OpenGL debug handler */
+    glDebugMessageCallback(gl_debug_proc, ctx);
 
     /* Initialize game state data */
     ctx->rotation = 0.0f;
--- demo/src/window.c
+++ demo/src/window.c
@@ -90,7 +90,7 @@ struct window* window_create(const char* title, int width, int height, int mode)
     /* Open GL version hints */
     glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
     glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
