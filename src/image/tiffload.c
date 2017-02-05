b38414661094cb7cdce589acc77370b5d513976f
TheArtist <agorglouk@gmail.com>
2017-02-06 01:05:01 +0200

    Set custom warning handler in tiffload to suspend warnings

--- src/image/tiffload.c
+++ src/image/tiffload.c
@@ -11,7 +11,8 @@ struct tiff_callback_data {
     unsigned char* cur_pos;
 };
 
-tsize_t tiff_read_cb(thandle_t handle, tdata_t buf, tsize_t sz) {
+tsize_t tiff_read_cb(thandle_t handle, tdata_t buf, tsize_t sz)
+{
     struct tiff_callback_data* cbdata = (struct tiff_callback_data*) handle;
     /* Read sz data to buf from cbdata->src */
     tsize_t available = (cbdata->src + cbdata->size) - cbdata->cur_pos;
@@ -21,17 +22,20 @@ tsize_t tiff_read_cb(thandle_t handle, tdata_t buf, tsize_t sz) {
     return sz;
 }
 
-tsize_t tiff_write_cb(thandle_t handle, tdata_t buf, tsize_t sz) {
+tsize_t tiff_write_cb(thandle_t handle, tdata_t buf, tsize_t sz)
+{
     (void) handle; (void) buf; (void) sz;
     return 0;
 }
 
-int tiff_close_cb(thandle_t handle) {
+int tiff_close_cb(thandle_t handle)
+{
     (void) handle;
     return 0;
 }
 
-toff_t tiff_seek_cb(thandle_t handle, toff_t off, int whence) {
+toff_t tiff_seek_cb(thandle_t handle, toff_t off, int whence)
+{
     struct tiff_callback_data* cbdata = (struct tiff_callback_data*) handle;
     switch(whence) {
         case SEEK_SET:
@@ -47,12 +51,27 @@ toff_t tiff_seek_cb(thandle_t handle, toff_t off, int whence) {
     return cbdata->cur_pos - cbdata->src;
 }
 
-toff_t tiff_sz_cb(thandle_t handle) {
+toff_t tiff_sz_cb(thandle_t handle)
+{
     struct tiff_callback_data* cbdata = (struct tiff_callback_data*) handle;
     return cbdata->size;
 }
 
-struct image* image_from_tiff(const unsigned char* data, size_t sz) {
+static void tiff_warn_handler(thandle_t th, const char* module, const char* fmt, va_list args)
+{
+    /* TODO: implement with future AssetLoader trace function */
+    (void) th;
+    (void) module;
+    (void) fmt;
+    (void) args;
+}
+
+struct image* image_from_tiff(const unsigned char* data, size_t sz)
+{
+    /* Set warning handler */
+    TIFFSetWarningHandler(0); /* Remove default first */
+    TIFFSetWarningHandlerExt(tiff_warn_handler);
+
     /* Fill read callback info */
     struct tiff_callback_data cbdata;
     cbdata.src = (unsigned char*) data;
